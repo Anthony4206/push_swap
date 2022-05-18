2c7565e48359c4ceb2749430681507209c2317e0
Anthony <anthony.levasseur@ymail.com>
2022-05-18 06:20:20 +0200

    maj rendu

--- srcs/ft_parsing.c
+++ srcs/ft_parsing.c
@@ -3,19 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ft_parsing.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alevasse <alevasse@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Anthony <Anthony@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 09:43:25 by alevasse          #+#    #+#             */
-/*   Updated: 2022/05/16 11:18:31 by alevasse         ###   ########.fr       */
+/*   Updated: 2022/05/17 22:22:35 by Anthony          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_is_error(t_list **lst)
+void	ft_is_error(t_list *lst)
 {
-	ft_lstclear(&(*lst));
+	ft_lstclear(&lst);
 	ft_putendl_fd("Error", STDERR_FILENO);
+	system("leaks push_swap");
 	exit(EXIT_FAILURE);
 }
 
@@ -31,7 +32,7 @@ static void	ft_check_digit(char *argv, t_list *ret)
 		while (argv[i])
 		{
 			if (!ft_isdigit(argv[i]) || !argv[i])
-				ft_is_error(&ret);
+				ft_is_error(ret);
 			i++;
 		}
 	}
@@ -49,7 +50,7 @@ static void	ft_check_double(t_list *ret)
 		while (cmp)
 		{
 			if (ret->value == cmp->value)
-				ft_is_error(&ret);
+				ft_is_error(ret);
 			cmp = cmp->next;
 		}
 		ret = ret->next;
@@ -74,29 +75,45 @@ int	ft_check_order(t_list *a)
 	return (size);
 }
 
-t_list	*ft_parsing(int argc, char **argv)
+
+
+t_list	*ft_parsing(int argc, char **argv, t_list *ret, int i)
 {
-	t_list		*ret;
-	int			i;
 	long int	nb;
 
-	ret = NULL;
-	i = 1;
+	if (!ft_strlen(argv[1]))
+		ft_is_error(ret);
 	if (argc == 2)
 	{
 		argv = ft_split(argv[1], ' ');
-		i = 0;
 	}
 	while (argv[i])
 	{
+		if (!ft_strlen(argv[i]))
+			ft_is_error(ret);
 		ft_check_digit(argv[i], ret);
 		nb = ft_atol(argv[i]);
 		if (nb > INT32_MAX || nb < INT32_MIN)
-			ft_is_error(&ret);
+			ft_is_error(ret);
 		ft_lstadd_back(&ret, ft_lstnew((int)nb));
 		i++;
-	}
+	}	
 	if (i > 1)
 		ft_check_double(ret);
+	return (ret);
+}
+
+t_list	*ft_add_parsing(int argc, char **argv)
+{
+	t_list	*ret;
+	
+	ret = NULL;
+	ft_printf("Coucou\n");
+	if (argc == 2)
+		ft_parsing(argc, ft_split(argv[1], ' '), ret, 0);
+	else
+		ft_parsing(argc, argv, ret, 1);
+	if (argc == 2)
+		ft_free_tab(argv, i);	
 	return (ret);
 }
